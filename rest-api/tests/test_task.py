2219c849cdb0218f56b090f2ce698ecbe873c035
reognust <a0939988526@gmail.com>
2022-04-24 17:41:33 +0800

    fix(test): put test config in current test file.

--- rest-api/tests/test_task.py
+++ rest-api/tests/test_task.py
@@ -1,4 +1,25 @@
 import json
+import pytest
+from app.app import create_app
+from app.data.task import TASKS
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+    
+    yield app
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
 
 
 def test_add_new_task(client):
@@ -6,21 +27,26 @@ def test_add_new_task(client):
     task = json.loads(response.data)
     assert task.get('name')  == "吃早餐"
 
-def test_search_task_by_id(client):
-    response = client.get("/task/0");
-    task = json.loads(response.data)
-    assert task.get('id') == 0
-
-def test_update_task_by_id(client):
-    response = client.put("/task/0", json={"name": "吃早餐", "status": 1});
-    task = json.loads(response.data)
-    assert task.get('status') == '1'
-
 def test_list_all_tasks(client):
+    response = client.post("/task", json={"name": "吃午餐"});
+    response = client.post("/task", json={"name": "吃晚餐"});
+    response = client.post("/task", json={"name": "吃宵夜"});
     response = client.get("/task")
     tasks = json.loads(response.data)
-    assert len(tasks) == 1
+    assert len(tasks) == 4
+
+def test_search_task_by_id(client):
+    response = client.post("/task", json={"name": "吃晚餐"});
+    response = client.get("/task/2");
+    task = json.loads(response.data)
+    assert task.get('id') == 2 and task.get("name") == "吃晚餐" and task.get("status") == 0
 
 def test_del_task_by_id(client):
-    response = client.delete("/task/0");
+    response = client.delete("/task/1");
     assert json.loads(response.data) == ""
+
+def test_update_task_by_id(client):
+    response = client.put("/task/0", json={"name": "吃早午餐", "status": 1});
+    task = json.loads(response.data)
+    assert task.get('status') == '1' and task.get("name") == "吃早午餐" 
+
